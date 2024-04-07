c1c018f105c69b9da38f929812254feedc020493
claude-perrin <vity5.diduk@gmail.com>
2024-04-06 19:25:37 -0600

    basic test lib

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,36 @@
 #![no_main]
 #![no_std]
+#![feature(custom_test_frameworks)]
+#![test_runner(catos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 
 use core::panic::PanicInfo;
-mod vga_buffer;
+use catos::println;
+use catos::serial_println;
 
-
-
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("MAIN panic");
+    catos::test_panic_handler(info)
+}
+
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World {}", "!");
+    serial_println!("MAIN _start");
+    #[cfg(test)]
+    test_main();
     loop {}
 }
-
 
 
